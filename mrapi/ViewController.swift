2bb63dc86d1bb53b981edb63c2ef532070bb9967
Sangamesh Hiremath <sangamesh.hiremath@standardbank.co.za>
2023-06-16 17:48:16 +0530

    Initial Commit

---
+++ mrapi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mrapi
+//
+//  Created by Sangamesh on 16/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
